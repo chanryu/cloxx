b34fe3c9038867679ac5d160a5be6cad98d65b2c
Chan Ryu <upriser@gmail.com>
2021-08-24 17:16:29 +1000

    CMAKE_BUILD_TYPE support (#3)

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -1,10 +1,49 @@
 #!/bin/bash
 
+#set -e
+
 PROJECT=$(git rev-parse --show-toplevel)
 BUILD="$PROJECT/build"
-if [ "$1" == "-r" ]; then
+
+BUILD_TYPE_FILE_PATH="$BUILD/BUILD_TYPE"
+BUILD_TYPE="Release"
+OLD_BUILD_TYPE=""
+
+CLEAN_FORCED=no
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -d|--debug)
+      BUILD_TYPE="Debug"
+      shift
+      ;;
+    -c|--clean)
+      CLEAN_FORCED=yes
+      shift
+      ;;
+    *) # unknown option - skip
+      shift
+      ;;
+  esac
+done
+
+
+buildtype_mismatch() {
+  if [ -f "$BUILD_TYPE_FILE_PATH" ]; then
+    OLD_BUILD_TYPE=`cat $BUILD_TYPE_FILE_PATH`
+    if [ "$OLD_BUILD_TYPE" == $BUILD_TYPE ]; then
+      return 1
+    fi
+  fi
+  return 0
+}
+
+if [ $CLEAN_FORCED == "yes" ] || buildtype_mismatch ; then
   rm -rf "$BUILD"
 fi
-mkdir -p "$BUILD" && pushd "$BUILD" > /dev/null && cmake  -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" .. && popd > /dev/null
 
-"$PROJECT/scripts/gen-ast.sh"
+# generate Makefiles
+mkdir -p "$BUILD" && pushd "$BUILD" && cmake  -DCMAKE_BUILD_TYPE=$BUILD_TYPE -G "Unix Makefiles" .. && popd > /dev/null
+
+# store current build type
+printf "$BUILD_TYPE" > "$BUILD_TYPE_FILE_PATH"
