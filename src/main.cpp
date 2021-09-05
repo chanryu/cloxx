f28bc2ca9aae41974ed5e0dec38b64de04caf33b
Chan Ryu <upriser@gmail.com>
2021-09-05 22:05:05 +1000

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -12,12 +12,22 @@ using namespace cloxx;
 
 int main(int argc, char const* argv[])
 {
-    if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " filepath\n";
+    if (argc > 2) {
+        std::cerr << "Usage: " << argv[0] << " [filepath]\n";
         return 1;
     }
 
-    int result = Lox{}.runFile(argv[1]);
+    int result;
+    {
+        Lox lox;
+
+        if (argc == 2) {
+            result = lox.runFile(argv[1]);
+        }
+        else {
+            result = lox.repl();
+        }
+    }
 
 #ifdef CLOXX_GC_DEBUG
     LOX_ASSERT(LoxObject::instanceCount() == 0);
