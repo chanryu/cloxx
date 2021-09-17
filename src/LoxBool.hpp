167977e5d1ef2d8e91dd26dbdd148bdd20ac62a0
Chan Ryu <upriser@gmail.com>
2021-09-17 16:59:30 +1000

    wip

--- src/LoxBool.hpp
+++ src/LoxBool.hpp
@@ -5,8 +5,10 @@
 namespace cloxx {
 
 class LoxClass;
+class LoxInstance;
 class GarbageCollector;
 
 std::shared_ptr<LoxClass> createBoolClass(GarbageCollector* gc);
+std::shared_ptr<LoxInstance> createBoolInstance(GarbageCollector* gc);
 
 } // namespace cloxx
